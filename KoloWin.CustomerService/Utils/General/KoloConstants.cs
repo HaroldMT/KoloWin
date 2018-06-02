d56fded14f46d5e89018c1d717a143c9434ad2e9
wilfried.tiani <wilfried.tiani@RES10_L>
2018-06-02 05:46:28 +0100

    - Create KoloConstants class

---
+++ KoloWin.CustomerService/Utils/General/KoloConstants.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KoloWin.CustomerService.Utils.General
+{
+    public static class KoloConstants
+    {
+        public static string TRANSFERT_ACCOUNT_TO_ACCOUNT = "TRANSFERTA2A";
+        public static string TRANSFERT_ACCOUNT_TO_CASH = "TRANSFERTA2C";
+        public static string TRANSFERT_CASH_TO_ACCOUNT = "TRANSFERTC2A";
+        public static string TRANSFERT_CASH_TO_CASH = "TRANSFERTC2C";
+
+        public static string TRANSFERT_TYPE_SEND_ACCOUNT_TO_ACCOUNT = "SENDA2A";
+        public static string TRANSFERT_TYPE_RECIEVE_ACCOUNT_TO_ACCOUNT = "RECVA2A";
+        public static string TRANSFERT_TYPE_SEND_ACCOUNT_TO_CASH = "SENDA2C";
+        public static string TRANSFERT_TYPE_RECIEVE_ACCOUNT_TO_CASH = "RECVA2C";
+        public static string TRANSFERT_TYPE_SEND_CASH_TO_ACCOUNT = "SENDC2A";
+        public static string TRANSFERT_TYPE_RECIEVE_CASH_TO_ACCOUNT = "RECVC2A";
+        public static string TRANSFERT_TYPE_SEND_CASH_TO_CASH = "SENDC2C";
+        public static string TRANSFERT_TYPE_RECIEVE_CASH_TO_CASH = "RECVC2C";
+        public static string TRANSFERT_TYPE_DEPOSIT = "DEPOSIT";
+        public static string TRANSFERT_TYPE_RECIEVE_WITHDRAWAL = "WITHDRAWAL";
+
+        public static string TRANSFERT_STATUS_CODE_RECEIVE = "RECEIVE";
+        public static string TRANSFERT_STATUS_CODE_SENDING = "SENDING";
+        public static string TRANSFERT_STATUS_CODE_SEND = "SEND";
+        public static string TRANSFERT_STATUS_CODE_RECEIVING = "RECEIVING";
+
+
+
+
+
+    }
+}
