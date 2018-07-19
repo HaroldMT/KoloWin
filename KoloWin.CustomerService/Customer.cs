20dff9c532bf072bf4f988402787f049896afa33
HaroldMT <tm.harold@gmail.com>
2018-07-19 16:34:29 +0100

    Database Update

--- KoloWin.CustomerService/Customer.cs
+++ KoloWin.CustomerService/Customer.cs
@@ -18,6 +18,7 @@ namespace KoloWin.CustomerService
         public Customer()
         {
             this.KoloNotifications = new ObservableCollection<KoloNotification>();
+            this.MobileDevices = new ObservableCollection<MobileDevice>();
         }
     
         public int IdCustomer { get; set; }
@@ -45,5 +46,7 @@ namespace KoloWin.CustomerService
         public virtual Person Person { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ObservableCollection<KoloNotification> KoloNotifications { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ObservableCollection<MobileDevice> MobileDevices { get; set; }
     }
 }
